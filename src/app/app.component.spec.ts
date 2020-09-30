0eb83599c03cd1b0c89c749755882a5ae8a9a967
Daniel Crha <daniel.crha@gmail.com>
2020-09-30 20:51:55 +0200

    Fix CI tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'yawnston-github-io'`, () => {
+  it(`should have as title 'yawnston.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('yawnston-github-io');
+    expect(app.title).toEqual('yawnston.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('yawnston-github-io app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('yawnston.github.io app is running!');
   });
 });
